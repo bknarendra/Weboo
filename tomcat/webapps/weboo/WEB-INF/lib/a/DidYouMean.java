db9c80562f1db52eef907d0088aa32416f9d3ed8
bknarendra <bknarendra2008@gmail.com>
2012-08-17 14:29:55 +0530

    Update tomcat/webapps/weboo/WEB-INF/lib/a/DidYouMean.java

--- tomcat/webapps/weboo/WEB-INF/lib/a/DidYouMean.java
+++ tomcat/webapps/weboo/WEB-INF/lib/a/DidYouMean.java
@@ -1,25 +1,41 @@
-package a;
 import java.io.*;
 import java.net.*;
 import org.jsoup.*;
+import java.util.zip.*;
 import org.jsoup.nodes.*;
+import org.jsoup.examples.HtmlToPlainText;
 public class DidYouMean {
 	public static String didYouMean(String s){
 		String word="";
 		String url="http://www.google.co.in/search?hl=en&q="+URLEncoder.encode(s);
 		String html=Utility.executeGet(url,"www.google.co.in",'i');
-		//System.out.println(html);
 		Document content=Jsoup.parse(html);
 		Element submitted=null;
 		try{
-			submitted=content.getElementById("ires").clone();
-			submitted=submitted.getElementsContainingText("Showing results for").first().clone();
-			submitted=submitted.getElementsByTag("a").first().clone();
-			word=submitted.text();
-		}catch(Exception e){}//System.out.println("error in did you mean");}	
+			submitted=content.getElementById("topstuff").clone();
+			HtmlToPlainText h=new HtmlToPlainText();
+			word=h.getPlainText(submitted);
+			int q,p=word.indexOf("Did you mean:");
+			if(p>=0){
+				word=word.substring(p+"Did you mean:".length());
+				p=word.indexOf("<>");
+				if(p>0) word=word.substring(0,p);
+				word=word.trim();
+			}
+			else{
+				p=word.indexOf("Showing results for");
+				if(p>=0){
+					word=word.substring(p+"Showing results for".length());
+					p=word.indexOf("<>");
+					if(p>0) word=word.substring(0,p);
+					word=word.trim();
+				}
+				else return "No results";
+			}
+		}catch(Exception e){e.printStackTrace();}	
 		return word;
 	}
 	public static void main(String args[]){
-		//System.out.println(didYouMean("footbll"));
+		System.out.println(didYouMean(args[0]));
 	}
 }
